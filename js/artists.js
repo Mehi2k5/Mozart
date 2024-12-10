465dc35266f4050e512e822c44ca0f67ece2077b
Xu <147921346+Mehi2k5@users.noreply.github.com>
2024-12-10 14:15:07 -0500

    Add files via upload

---
+++ js/artists.js
@@ -0,0 +1,57 @@
+/**
+ * artists.js
+ *
+ * The app's list of Artists
+ * 
+ * 
+- Heart on my sleeve
+    - ghostwriter977
+    - April 2023
+    - Drake/Weeknd
+- Trappin 4 the amish 
+    - Banned records 
+    - May 6 2024
+    - new
+- Push it down 
+    - H.A.L.O.
+    - May 12 2024
+    - Kid cudi? 
+- Forever 
+    - Corwii
+    - May 1 2024
+    - Post malone?
+- Gold Hands
+    - Alabasta 
+    - Future
+    - May 18 2024
+ */
+
+
+// 
+window.artists = [
+  {
+    artistID: 1,
+    name: "ghostwriter977",
+    profilePic: "/images/ghostwriter.jpg",
+  },
+  {
+    artistID: 2,
+    name: "Corwii",
+    profilePic: "/images/corwii.webp",
+  },
+  {
+    artistID: 3,
+    name: "Alabasta",
+    profilePic: "/images/unnamed.png",
+  },
+  {
+    artistID: 4,
+    name: "H.A.L.O.",
+    profilePic: "/images/halo.jpg",
+  },
+  // {
+  //   artistID: 5,
+  //   name: "Banned Records",
+  //   profilePic: "/images/bannedrecords.jpg",
+  // }
+];
